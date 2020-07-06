2edee0c47478384f84e005c1b1ad11a46222f08c
Daniel Kane <dpk2029@gmail.com>
2020-07-06 00:55:33 -0400

    Nodes are attracted to mouse for nice animation affect

--- src/components/LinkVisualizer/LinkVisualizer.js
+++ src/components/LinkVisualizer/LinkVisualizer.js
@@ -31,19 +31,27 @@ class LinkVisualizer extends Component {
   componentDidMount() {
     this.scene = new THREE.Scene();
     let orgContainer = document.getElementById('link-visualizer-container');
+
     this.aspectRatio = orgContainer.offsetWidth / orgContainer.offsetHeight;
     this.camera = new THREE.OrthographicCamera(-this.aspectRatio, this.aspectRatio, 1, -1, 1, -1);
+
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize(orgContainer.offsetWidth, orgContainer.offsetHeight);
     
     window.addEventListener('resize', () => {
-      let container = document.getElementById('link-visualizer-container');
-      this.prevAspectRatio = this.aspectRatio;
-      this.aspectRatio = container.offsetWidth / container.offsetHeight;
-      this.renderer.setSize(container.offsetWidth, container.offsetHeight);
-      this.updateScene();
+      this.resizeCamera();
+      this.resizeLink();
+    });
+
+    this.mouse = new THREE.Vector3();
+    orgContainer.addEventListener('mousemove', (event) => {
+      let canvasBounds = orgContainer.getBoundingClientRect();
+      this.mouse.x = 2 * this.aspectRatio * (((event.clientX - canvasBounds.left) / (canvasBounds.right - canvasBounds.left)) - 0.5);
+      this.mouse.y = -2 * (((event.clientY - canvasBounds.top) / (canvasBounds.bottom - canvasBounds.top)) - 0.5);
     });
     
+    this.mousePullRange = 0.3;
+    this.mousePullStrength = 0.25;
 
     this.mount.appendChild(this.renderer.domElement);
     
@@ -51,27 +59,70 @@ class LinkVisualizer extends Component {
 
     var animate = () => {
       requestAnimationFrame( animate );
+      this.updateScene();
       this.renderer.render( this.scene, this.camera );
     };
+
     animate();
   }
 
-  updateScene() {
-    this.camera.right = this.aspectRatio;
-    this.camera.left = -this.aspectRatio;
-    this.camera.updateProjectionMatrix();
-    
-    this.updateLink();
+  applyMouseForce(node) {
+    let mouseDist = this.mouse.distanceTo(node.userData.origPos);
+    let nodeTravelDist = node.position.distanceTo(node.userData.origPos);
+
+    if (mouseDist < this.mousePullRange && nodeTravelDist < this.mousePullRange) {
+      let pullStrength = Math.pow(this.mousePullRange - mouseDist, 2);
+      node.position.lerp(this.mouse, pullStrength);
+    } else {
+      node.position.lerp(node.userData.origPos, 0.1);
+    }
   }
 
-  updateLink() {
+  updateScene() {
+    for (let index = 0; index < linkData.length; index++) {
+      let node = this.scene.getObjectByName(`node-${index}`);
+      this.applyMouseForce(node);
+    }
+
+    for (let index = 1; index < linkData.length; index++) {
+      let line = this.scene.getObjectByName(`line-${index-1}:${index}`);
+      let prevNode = this.scene.getObjectByName(`node-${index-1}`);
+      let node = this.scene.getObjectByName(`node-${index}`);
+        
+      line.geometry = new THREE.BufferGeometry().setFromPoints([
+        new THREE.Vector3(prevNode.position.x, prevNode.position.y, 0.5),
+        new THREE.Vector3(node.position.x,     node.position.y,     0.5),
+      ]);
+    }
+  }
+
+  resizeCamera() {
+    let container = document.getElementById('link-visualizer-container');
+
+    this.prevAspectRatio = this.aspectRatio;
+    this.aspectRatio = container.offsetWidth / container.offsetHeight;
+
+    this.renderer.setSize(container.offsetWidth, container.offsetHeight);
+
+    this.camera.right = this.aspectRatio;
+    this.camera.left = -this.aspectRatio;
+
+    this.camera.updateProjectionMatrix();
+  }
+
+  resizeLink() {
     const colWidth = 2 * this.aspectRatio / linkData.length;
 
     let prevNode = undefined;
     for (let index = 0; index < linkData.length; index++) {
       let node = this.scene.getObjectByName(`node-${index}`);
+
       node.position.x = this.aspectRatio * node.position.x / this.prevAspectRatio;
+      node.userData.origPos.x = node.position.x;
+
       node.scale.set(colWidth/4, colWidth/4, 1);
+
+      this.applyMouseForce(node);
       
       if (prevNode !== undefined) {
         let line = this.scene.getObjectByName(`line-${index-1}:${index}`);
@@ -81,6 +132,7 @@ class LinkVisualizer extends Component {
           new THREE.Vector3(node.position.x,     node.position.y,     0.5),
         ]);
       }
+
       prevNode = node;
     }
   }
@@ -94,6 +146,7 @@ class LinkVisualizer extends Component {
         new THREE.CircleGeometry(1, 64), 
         new THREE.MeshBasicMaterial({ color: 0xffff00 })
       );
+
       circle.scale.set(colWidth/4, colWidth/4, 1);
       circle.name = `node-${index}`;
 
@@ -102,6 +155,7 @@ class LinkVisualizer extends Component {
       const offsetY = this.randRange(-colWidth/4, colWidth/4);
 
       circle.position.set(x + offsetX, offsetY, 0);
+      circle.userData.origPos = new THREE.Vector3(x + offsetX, offsetY, 0);
       this.scene.add(circle);
 
       if (prevCirclePos !== undefined) {
@@ -112,9 +166,10 @@ class LinkVisualizer extends Component {
           ]),
           new THREE.LineBasicMaterial({
             color: 0xffffff,
-            linewidth: 5,
+            linewidth: 50,
           })
         );
+
         line.name = `line-${index-1}:${index}`;
 
         this.scene.add(line);
