1422f0f77fd44e177f4e78e047ec82e5629bef0e
Daniel Kane <dpk2029@gmail.com>
2020-07-06 21:11:47 -0400

    Changed line segments to continuous curve with thicker lines

--- src/components/LinkVisualizer/LinkVisualizer.js
+++ src/components/LinkVisualizer/LinkVisualizer.js
@@ -1,27 +1,33 @@
 import React, { Component } from 'react';
 import * as THREE from 'three';
+import MeshLine, { MeshLineMaterial } from 'three.meshline';
 import './LinkVisualizer.css';
 
 var linkData = [
   {
     type: "ACTOR",
-    name: "ACTOR 1"
+    name: "ACTOR 1",
+    imageURL: "https://image.tmdb.org/t/p/w500/d0ZMdgMz1mVcWWctyF7sbymSlv4.jpg"
   },
   {
     type: "MOVIE",
-    name: "MOVIE 1"
+    name: "MOVIE 1",
+    imageURL: "https://image.tmdb.org/t/p/w500/d0ZMdgMz1mVcWWctyF7sbymSlv4.jpg"
   },
   {
     type: "ACTOR",
-    name: "ACTOR 2"
+    name: "ACTOR 2",
+    imageURL: "https://image.tmdb.org/t/p/w500/d0ZMdgMz1mVcWWctyF7sbymSlv4.jpg"
   },
   {
     type: "MOVIE",
-    name: "MOVIE 2"
+    name: "MOVIE 2",
+    imageURL: "https://image.tmdb.org/t/p/w500/d0ZMdgMz1mVcWWctyF7sbymSlv4.jpg"
   },
   {
     type: "ACTOR",
-    name: "ACTOR 3"
+    name: "ACTOR 3",
+    imageURL: "https://image.tmdb.org/t/p/w500/d0ZMdgMz1mVcWWctyF7sbymSlv4.jpg"
   },
 ];
 
@@ -43,15 +49,16 @@ class LinkVisualizer extends Component {
       this.resizeLink();
     });
 
+
+    this.mousePullStrength = 0.5;
     this.mouse = new THREE.Vector3();
+    this.mouseSet = false;
     orgContainer.addEventListener('mousemove', (event) => {
+      this.mouseSet = true;
       let canvasBounds = orgContainer.getBoundingClientRect();
       this.mouse.x = 2 * this.aspectRatio * (((event.clientX - canvasBounds.left) / (canvasBounds.right - canvasBounds.left)) - 0.5);
       this.mouse.y = -2 * (((event.clientY - canvasBounds.top) / (canvasBounds.bottom - canvasBounds.top)) - 0.5);
     });
-    
-    this.mousePullRange = 0.3;
-    this.mousePullStrength = 0.25;
 
     this.mount.appendChild(this.renderer.domElement);
     
@@ -60,13 +67,15 @@ class LinkVisualizer extends Component {
     var animate = () => {
       requestAnimationFrame( animate );
       this.updateScene();
-      this.renderer.render( this.scene, this.camera );
+      this.renderer.render(this.scene, this.camera);
     };
 
     animate();
   }
 
   applyMouseForce(node) {
+    if (!this.mouseSet) return;
+
     let mouseDist = this.mouse.distanceTo(node.userData.origPos);
     let nodeTravelDist = node.position.distanceTo(node.userData.origPos);
 
@@ -78,22 +87,39 @@ class LinkVisualizer extends Component {
     }
   }
 
+  createCurve(controlPoints) {
+    let curvePoints = new THREE.CatmullRomCurve3(controlPoints)
+      .getPoints(linkData.length * 32)
+      .map((point) => {return [point.x, point.y, point.z]})
+      .flat();
+
+    let meshLine = new MeshLine.MeshLine();
+    meshLine.setGeometry(curvePoints);
+    let size = new THREE.Vector2();
+    this.renderer.getSize(size)
+    let lineMaterial = new MeshLineMaterial({
+      color: new THREE.Color(1, 1, 1),
+      resolution: size,
+      lineWidth: 0.01
+    });
+
+    let curve = new THREE.Mesh(meshLine.geometry, lineMaterial);
+    curve.name = 'curve';
+
+    return curve;
+  }
+
   updateScene() {
-    for (let index = 0; index < linkData.length; index++) {
+    let curveControlPoints = linkData.map((_, index) => {
       let node = this.scene.getObjectByName(`node-${index}`);
       this.applyMouseForce(node);
-    }
-
-    for (let index = 1; index < linkData.length; index++) {
-      let line = this.scene.getObjectByName(`line-${index-1}:${index}`);
-      let prevNode = this.scene.getObjectByName(`node-${index-1}`);
-      let node = this.scene.getObjectByName(`node-${index}`);
-        
-      line.geometry = new THREE.BufferGeometry().setFromPoints([
-        new THREE.Vector3(prevNode.position.x, prevNode.position.y, 0.5),
-        new THREE.Vector3(node.position.x,     node.position.y,     0.5),
-      ]);
-    }
+      return node.position;
+    });
+    
+    let curve = this.scene.getObjectByName('curve');
+    this.scene.remove(curve);
+    this.scene.add(this.createCurve(curveControlPoints));
+    
   }
 
   resizeCamera() {
@@ -112,71 +138,54 @@ class LinkVisualizer extends Component {
 
   resizeLink() {
     const colWidth = 2 * this.aspectRatio / linkData.length;
+    this.mousePullRange = colWidth / 2;
 
-    let prevNode = undefined;
-    for (let index = 0; index < linkData.length; index++) {
-      let node = this.scene.getObjectByName(`node-${index}`);
 
-      node.position.x = this.aspectRatio * node.position.x / this.prevAspectRatio;
-      node.userData.origPos.x = node.position.x;
+    linkData.forEach((node, index) => {
+      let circle = this.scene.getObjectByName(`node-${index}`);
 
-      node.scale.set(colWidth/4, colWidth/4, 1);
+      circle.position.x = this.aspectRatio * circle.position.x / this.prevAspectRatio;
+      circle.userData.origPos.x = circle.position.x;
 
-      this.applyMouseForce(node);
-      
-      if (prevNode !== undefined) {
-        let line = this.scene.getObjectByName(`line-${index-1}:${index}`);
-        
-        line.geometry = new THREE.BufferGeometry().setFromPoints([
-          new THREE.Vector3(prevNode.position.x, prevNode.position.y, 0.5),
-          new THREE.Vector3(node.position.x,     node.position.y,     0.5),
-        ]);
-      }
+      circle.scale.set(colWidth/4, colWidth/4, 1);
 
-      prevNode = node;
-    }
+      this.applyMouseForce(circle);
+    })
   }
 
   createLink(link) {
     const colWidth = 2 * this.aspectRatio / link.length;
+    this.mousePullRange = colWidth / 2;
 
-    let prevCirclePos = undefined;
+    let loader = new THREE.TextureLoader();
+
     link.forEach((node, index) => {
       var circle = new THREE.Mesh(
         new THREE.CircleGeometry(1, 64), 
-        new THREE.MeshBasicMaterial({ color: 0xffff00 })
+        new THREE.MeshBasicMaterial({ 
+          color: 0xffffff,
+          map: loader.load(node.imageURL)
+        })
       );
-
+      
       circle.scale.set(colWidth/4, colWidth/4, 1);
       circle.name = `node-${index}`;
 
       const x = colWidth * index + colWidth/2 - this.aspectRatio; 
       const offsetX = this.randRange(-colWidth/8, colWidth/8);
-      const offsetY = this.randRange(-colWidth/4, colWidth/4);
+      const offsetY = this.randRange(0.33, -0.33);
 
       circle.position.set(x + offsetX, offsetY, 0);
       circle.userData.origPos = new THREE.Vector3(x + offsetX, offsetY, 0);
       this.scene.add(circle);
-
-      if (prevCirclePos !== undefined) {
-        let line = new THREE.Line(
-          new THREE.BufferGeometry().setFromPoints([
-            new THREE.Vector3(prevCirclePos.x, prevCirclePos.y, 0.5), 
-            new THREE.Vector3(x + offsetX, offsetY, 0.5)
-          ]),
-          new THREE.LineBasicMaterial({
-            color: 0xffffff,
-            linewidth: 50,
-          })
-        );
-
-        line.name = `line-${index-1}:${index}`;
-
-        this.scene.add(line);
-      }
-
-      prevCirclePos = new THREE.Vector3(x + offsetX, offsetY, 0);
     });
+
+    let curveControlPoints = this.scene.children.map((obj) => {
+      return new THREE.Vector3(obj.position.x, obj.position.y, 0.5);
+    });
+
+    let curve = this.createCurve(curveControlPoints);
+    this.scene.add(curve);
 
     this.renderer.render(this.scene, this.camera);
   }
